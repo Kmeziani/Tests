545411601a485985f964ad77590c9b639d9c276d
Kahina MEZIANI <Kahina@bst.local>
2025-03-20 16:31:38 +0100

    Ajout du fichier bashup.sh

---
+++ bashup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Ajouter tous les fichiers modifiés
+git add .
+
+# Commit avec un message
+git commit -m "Mise à jour automatique"
+
+# Pousser les changements vers GitHub
+git push origin main
